d1ad0a8276ec3661d2472cdd5538f0050bfe4e74
leo <leo.gms1995@gmail.com>
2026-01-07 23:14:28 -0300

    Primeiro commit

---
+++ js/data/estacoesSP.js
@@ -0,0 +1,9 @@
+const tabela_estacoes_pluviometricas_sp = [
+    { estacao_pluviometrica: 'Campo do Paiol', latitude: `23°23'00"`, longitude: `48°02'00"` },
+    { estacao_pluviometrica: 'Salto de Pirapora', latitude: `23°38'18"`, longitude: `47°34'23"`},
+    { estacao_pluviometrica: 'Fazenda Pinhal', latitude: `23°16'00"`, longitude: `47°07'00"`},
+    { estacao_pluviometrica: 'Santa Gertrudes', latitude: `22°29'00"`, longitude: `47°31'00"`},
+    { estacao_pluviometrica: 'Guará', latitude: `20°25'00"`, longitude: `47°49'00"`},
+    { estacao_pluviometrica: 'Paretaí', latitude: `23°18'53"`, longitude: `46°07'00.59"`},
+    { estacao_pluviometrica: 'Jacareí', latitude: `23°17'00"`, longitude: `45°57'00"`}
+]
---
+++ js/services/calculos.js
@@ -0,0 +1,20 @@
+//import { estacoes } from "../data/estacoes.js";
+//import { Estacao } from "../models/Estacao.js";
+
+const longitude = document.getElementById("longitude")
+const latitude = document.getElementById("latitude")
+const area_bacia_contribuicao = document.getElementById("Ab")
+const comprimento_talvegue = document.getElementById("Lt")
+const desnivel_geometrico = document.getElementById("Hg")
+const coeficiente_runoff = document.getElementById("runoff")
+const periodo_retorno = document.getElementById("Tr")
+const declividade_longitudinal = document.getElementById("I")
+const declividade_transversal = document.getElementById("s")
+const largura_enxurrada_max = document.getElementById("Tmax")
+const coeficiente_manning = document.getElementById("n")
+const A = document.getElementById("A")
+const B = document.getElementById("B")
+const C = document.getElementById("C")
+const D = document.getElementById("D")
+const E = document.getElementById("E")
+const F = document.getElementById("F")
